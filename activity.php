10f5179195e69b58c9837cf81be4b6b2e2ab57ce
Java Jay <154245504+TadashiJei@users.noreply.github.com>
2024-03-20 20:43:47 +0800

    Update activity.php

--- activity.php
+++ activity.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'account-activity-log' => 'Account Activity Log',
-    'activity-log' => 'Activity Log',
-    'clear-filters' => 'Clear Filters',
+    'account-activity-log' => 'Tala ng Aktibidad ng Account',
+    'activity-log' => 'Tala ng Aktibidad',
+    'clear-filters' => 'Burahin ang Mga Filter',
     'metadata' => 'Metadata',
-    'close' => 'Close',
-    'no-logs-server' => 'No activity logs available for this server.'
+    'close' => 'Isara',
+    'no-logs-server' => 'Walang mga log ng aktibidad na available para sa server na ito.'
 ];
