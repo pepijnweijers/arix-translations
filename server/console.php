8b789ee40d455c6832b3d43fc5edfb6bc7761b24
Java Jay <154245504+TadashiJei@users.noreply.github.com>
2024-03-20 20:45:05 +0800

    Update console.php

--- server/console.php
+++ server/console.php
@@ -1,22 +1,38 @@
 <?php
 
 return [
-    'dashboard' => 'Dashboard',
+    'databases' => 'Mga Database',
+    'manage-databases' => 'Pamahalaan ang mga database',
+    'new-database' => 'Bagong Database', 
+    'have-been-allocated' => '{{current}} sa {{max}} na mga database ang na-allocate sa server na ito.',
+    'name' => 'Pangalan',
+    'username' => 'Username',
+    'endpoint' => 'Endpoint',
+    'details' => 'Mga Detalye',
+    'connection-from' => 'Koneksyon mula sa', 
+    'password' => 'Password',
+    'connection-string' => 'JDBC Connection String', 
+    'database-connection-details' => 'Mga detalye ng koneksyon sa database',
+    'no-databases' => 'Mukhang wala kang mga database.',
+    'cannot-be-created' => 'Hindi makakagawa ng mga database para sa server na ito.', 
+    'confirm-deletion' => 'Kumpirmahin ang pagtanggal ng database',
+    'delete-description-1' => 'Ang pagtanggal ng database ay isang permanenteng aksyon, hindi ito maaaring i-undo. Permanente nitong tatanggalin ang',
+    'delete-description-2' => 'database at aalisin ang lahat ng nauugnay na data.',
+    'confirm-database-name' => 'Kumpirmahin ang Pangalan ng Database', 
+    'confirm-database-description' => 'Ilagay ang pangalan ng database para kumpirmahin ang pagtanggal.',
+    'delete-database' => 'Tanggalin ang Database',
+    'rotate-password' => 'Paikutin ang Password', 
+    'database-details' => 'Mga Detalye ng Database',
+    'cancel' => 'Kanselahin',
+    'close' => 'Isara',
 
-    'sftp' => [
-        'title' => 'Mga Detalye ng SFTP', 
-        'server-address' => 'Address ng Server',
-        'username' => 'Username',
-        'description' => 'Ang iyong SFTP password ay pareho sa password na ginagamit mo para ma-access ang panel na ito.', 
-        'launch-sftp' => 'Ilunsad ang SFTP', 
-    ],
-    'server-info' => [
-        'title' => 'Impormasyon ng Server', 
-        'hostname' => 'Hostname',
-        'node-id' => 'Node ID',
-        'server-id' => 'Server ID',
-        
-        'uptime' => 'Uptime',
-        'server-ip' => 'Server IP',
-    ],
+    'create' => [
+        'title' => 'Gumawa ng bagong database',
+        'name' => 'Pangalan ng Database', 
+        'name-description' => 'Isang deskriptibong pangalan para sa iyong database instance.',
+        'connection-from' => 'Koneksyon Mula Sa', 
+        'connection-from-description' => 'Kung saan dapat payagan ang mga koneksyon. Mag-iwan ng blangko upang payagan ang mga koneksyon mula sa kahit saan.', 
+        'cancel' => 'Kanselahin',
+        'create-database' => 'Gumawa ng Database' 
+    ]
 ];
