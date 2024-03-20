68b53b217a0ed6d92172ddc904435369bf7ab0fa
Java Jay <154245504+TadashiJei@users.noreply.github.com>
2024-03-20 20:44:53 +0800

    Update console.php

--- server/console.php
+++ server/console.php
@@ -1,9 +1,22 @@
 <?php
 
 return [
-    'console' => 'Console',
-    'node-under-maintenance' => 'Ang node ng server na ito ay kasalukuyang nasa ilalim ng maintenance at hindi available ang lahat ng actions.', 
-    'running-installation-process' => 'Ang server na ito ay kasalukuyang nagpapatakbo ng proseso ng pag-install nito at karamihan sa mga actions ay hindi available.', 
-    'being-transferred' => 'Ang server na ito ay kasalukuyang nililipat sa ibang node at lahat ng actions ay hindi available.',
-    'type-a-command' => 'Mag-type ng command...', 
+    'dashboard' => 'Dashboard',
+
+    'sftp' => [
+        'title' => 'Mga Detalye ng SFTP', 
+        'server-address' => 'Address ng Server',
+        'username' => 'Username',
+        'description' => 'Ang iyong SFTP password ay pareho sa password na ginagamit mo para ma-access ang panel na ito.', 
+        'launch-sftp' => 'Ilunsad ang SFTP', 
+    ],
+    'server-info' => [
+        'title' => 'Impormasyon ng Server', 
+        'hostname' => 'Hostname',
+        'node-id' => 'Node ID',
+        'server-id' => 'Server ID',
+        
+        'uptime' => 'Uptime',
+        'server-ip' => 'Server IP',
+    ],
 ];
