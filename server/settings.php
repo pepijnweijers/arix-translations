e678f363ef3e5615fa85202d9e8c3d2901558283
Java Jay <154245504+TadashiJei@users.noreply.github.com>
2024-03-20 20:47:31 +0800

    Update settings.php

--- server/settings.php
+++ server/settings.php
@@ -1,37 +1,37 @@
 <?php
 
 return [
-    'settings' => 'Settings',
+    'settings' => 'Mga Setting',
 
     'sftp' => [
-        'title' => 'SFTP Details',
-        'server-address' => 'Server Address',
+        'title' => 'Mga Detalye ng SFTP', 
+        'server-address' => 'Address ng Server', 
         'username' => 'Username',
-        'description' => 'Your SFTP password is the same as the password you use to access this panel.',
-        'launch-sftp' => 'Launch SFTP',
+        'description' => 'Ang iyong SFTP password ay pareho sa password na ginagamit mo para ma-access ang panel na ito.', 
+        'launch-sftp' => 'Ilunsad ang SFTP', 
     ],
 
     'rename' => [
-        'title' => 'Change Server Details',
-        'server-name' => 'Server Name',
-        'server-description' => 'Server Description',
-        'save' => 'Save',
+        'title' => 'Baguhin ang mga Detalye ng Server', 
+        'server-name' => 'Pangalan ng Server',
+        'server-description' => 'Paglalarawan ng Server', 
+        'save' => 'I-save', 
     ],
 
     'debug' => [
-        'title' => 'Debug Information',
+        'title' => 'Impormasyon ng Debug',
         'node' => 'Node',
-        'server-id' => 'Server ID',
+        'server-id' => 'Server ID', 
     ],
 
     'reinstall' => [
-        'title' => 'Reinstall Server',
-        'reinstall-started' => 'Your server has begun the reinstallation process.',
-        'confirm-reinstallation' => 'Confirm server reinstallation',
-        'yes-reinstall' => 'Yes, reinstall server',
-        'are-you-sure' => 'Your server will be stopped and some files may be deleted or modified during this process, are you sure you wish to continue?',
-        'desc-1' => 'Reinstalling your server will stop it, and then re-run the installation script that initially set it up.',
-        'desc-2' => 'Some files may be deleted or modified during this process, please back up your data before continuing.',
-        'reinstall-server' => 'Reinstall Server',
+        'title' => 'I-reinstall ang Server',
+        'reinstall-started' => 'Nagsimula na ang proseso ng reinstallation ng iyong server.', 
+        'confirm-reinstallation' => 'Kumpirmahin ang reinstallation ng server', 
+        'yes-reinstall' => 'Oo, i-reinstall ang server', 
+        'are-you-sure' => 'Hihinto ang iyong server at maaaring mabura o mabago ang ilang mga file sa prosesong ito. Sigurado ka bang gusto mong magpatuloy?', 
+        'desc-1' => 'Ang pag-reinstall ng iyong server ay hihinto ito, at pagkatapos ay muling tatakbo ang installation script na unang na-set up ito.', 
+        'desc-2' => 'Maaaring mabura o mabago ang ilang mga file sa prosesong ito, mangyaring i-back up ang iyong files bago magpatuloy.',
+        'reinstall-server' => 'I-reinstall ang Server', 
     ],
 ];
