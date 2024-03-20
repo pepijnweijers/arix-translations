1121a0f016e4c98b572fe8f08689d40d663a6589
Java Jay <154245504+TadashiJei@users.noreply.github.com>
2024-03-20 20:45:24 +0800

    Update console.php

--- server/console.php
+++ server/console.php
@@ -1,38 +1,9 @@
 <?php
 
 return [
-    'databases' => 'Mga Database',
-    'manage-databases' => 'Pamahalaan ang mga database',
-    'new-database' => 'Bagong Database', 
-    'have-been-allocated' => '{{current}} sa {{max}} na mga database ang na-allocate sa server na ito.',
-    'name' => 'Pangalan',
-    'username' => 'Username',
-    'endpoint' => 'Endpoint',
-    'details' => 'Mga Detalye',
-    'connection-from' => 'Koneksyon mula sa', 
-    'password' => 'Password',
-    'connection-string' => 'JDBC Connection String', 
-    'database-connection-details' => 'Mga detalye ng koneksyon sa database',
-    'no-databases' => 'Mukhang wala kang mga database.',
-    'cannot-be-created' => 'Hindi makakagawa ng mga database para sa server na ito.', 
-    'confirm-deletion' => 'Kumpirmahin ang pagtanggal ng database',
-    'delete-description-1' => 'Ang pagtanggal ng database ay isang permanenteng aksyon, hindi ito maaaring i-undo. Permanente nitong tatanggalin ang',
-    'delete-description-2' => 'database at aalisin ang lahat ng nauugnay na data.',
-    'confirm-database-name' => 'Kumpirmahin ang Pangalan ng Database', 
-    'confirm-database-description' => 'Ilagay ang pangalan ng database para kumpirmahin ang pagtanggal.',
-    'delete-database' => 'Tanggalin ang Database',
-    'rotate-password' => 'Paikutin ang Password', 
-    'database-details' => 'Mga Detalye ng Database',
-    'cancel' => 'Kanselahin',
-    'close' => 'Isara',
-
-    'create' => [
-        'title' => 'Gumawa ng bagong database',
-        'name' => 'Pangalan ng Database', 
-        'name-description' => 'Isang deskriptibong pangalan para sa iyong database instance.',
-        'connection-from' => 'Koneksyon Mula Sa', 
-        'connection-from-description' => 'Kung saan dapat payagan ang mga koneksyon. Mag-iwan ng blangko upang payagan ang mga koneksyon mula sa kahit saan.', 
-        'cancel' => 'Kanselahin',
-        'create-database' => 'Gumawa ng Database' 
-    ]
+    'console' => 'Console',
+    'node-under-maintenance' => 'Ang node ng server na ito ay kasalukuyang nasa ilalim ng maintenance at hindi available ang lahat ng actions.', 
+    'running-installation-process' => 'Ang server na ito ay kasalukuyang nagpapatakbo ng proseso ng pag-install nito at karamihan sa mga actions ay hindi available.', 
+    'being-transferred' => 'Ang server na ito ay kasalukuyang nililipat sa ibang node at lahat ng actions ay hindi available.',
+    'type-a-command' => 'Mag-type ng command...', 
 ];
