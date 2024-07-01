60845dbad611d02f0c263899d40c6106a832d042
Its-Krzysztof <77639981+Its-Krzysztof@users.noreply.github.com>
2024-07-01 19:07:47 +0200

    Update startup.php

--- server/startup.php
+++ server/startup.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'startup-settings' => 'Startup Settings',
-    'startup-command' => 'Startup Command',
-    'docker-image' => 'Docker Image',
-    'read-only' => 'Read Only',
-    'select-docker-feature' => 'This is an advanced feature allowing you to select a Docker image to use when running this server instance.',
-    'custom-docker-image' => 'This {"server\'s"} Docker image has been manually set by an administrator and cannot be changed through this UI.',
-    'variables' => 'Variables'
+    'startup-settings' => 'Parametry startowe',
+    'startup-command' => 'Komenda startowa',
+    'docker-image' => 'Obraz Docker',
+    'read-only' => 'Tylko do odczytu',
+    'select-docker-feature' => 'Jest to zaawansowana funkcja umożliwiająca wybór obrazu Docker do użycia podczas uruchamiania tego serwera.',
+    'custom-docker-image' => 'Obraz Docker tego {"serwera"} został ręcznie ustawiony przez administratora i nie może być zmieniany za pomocą tego interfejsu użytkownika.',
+    'variables' => 'Parametry'
 ];
